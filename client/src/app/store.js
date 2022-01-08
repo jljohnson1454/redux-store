04660a559013649bce666e8c1ff1844fb728df16
Ghidorah <jljohnson1454@gmail.com>
2022-01-08 17:55:31 -0600

    changed configureStore to createStore

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { createStore } from '@reduxjs/toolkit';
+import reducer from '../utils/reducers';
 
-export default configureStore({
-    reducer: {
-        counter: counterReducer,
-    },
-})
+const store = createStore(reducer)
+
+export default store;
