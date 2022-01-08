8c9ebbbdc17fae68d0420f8247da3fbc00310015
Ghidorah <jljohnson1454@gmail.com>
2022-01-08 17:47:25 -0600

    added Redux sytax

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './app/store'
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
